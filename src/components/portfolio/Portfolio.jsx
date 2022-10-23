1e0787d6be63f76ab348c17201ddcf46a1b611ed
RobertRish <robert.rish1225@gmail.com>
2022-10-23 19:52:46 -0400

    add new projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,8 @@ import SNACKR from "../../assets/SNACKR.png";
 import SaySomething from "../../assets/SaySomething.png";
 import EmployeeTracker from "../../assets/employee-tracker.png";
 import ReadMeGenerator from "../../assets/readme.png";
+import EtchASketch from "../../assets/etch.png";
+import Quiz from "../../assets/quiz.png";
 
 const Portfolio = () => {
   return (
@@ -14,12 +16,65 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image">
+            <img src={EtchASketch} alt="Etch-a-Sketch application page" />
+          </div>
+          <h3>Etch-a-Sketch</h3>
+          <p className="tech-used">(HTML, CSS, JavaScript, Google Fonts API)</p>
+          <p>
+            A sketch pad application that allows users to choose the level of
+            pixelation thereof. 
+          </p>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/RobertRish/etch-a-sketch" className="btn">
+              GitHub
+            </a>
+            <a
+              href="https://robertrish.github.io/etch-a-sketch/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Deployed Project
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Quiz} alt="SaySomething home page" />
+          </div>
+          <h3>Multiple Choice Quiz</h3>
+          <p className="tech-used">
+            (HTML, CSS, JavaScript, Google Fonts API)
+          </p>
+          <p>A quiz application that utilizes client-side storage to save quiz scores.</p>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/RobertRish/quiz-js"
+              className="btn"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://robertrish.github.io/quiz-js/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Deployed Project
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
             <img src={SNACKR} alt="SNACKR home page" />
           </div>
           <h3>SNACKR</h3>
           <p className="tech-used">(HTML, CSS, JavaScript, Server-side API)</p>
           <p>
-            An application that allows users to search for dishes from a recipe API and then drag and drop recipe cards into a weekly meal planner.
+            An application that allows users to search for dishes from a recipe
+            API and then drag and drop recipe cards into a weekly meal planner.
           </p>
           <div className="portfolio__item-cta">
             <a href="https://github.com/nsvoboda/SNACKR" className="btn">
@@ -41,10 +96,11 @@ const Portfolio = () => {
             <img src={SaySomething} alt="SaySomething home page" />
           </div>
           <h3>SaySomething</h3>
-          <p className="tech-used">(HTML, CSS, JavaScript, Server-side API, Node, Express, SQL, Sequelize (ORM), Handlebars.js)</p>
-          <p>
-            A social media application.
+          <p className="tech-used">
+            (HTML, CSS, JavaScript, Server-side API, Node, Express, SQL,
+            Sequelize (ORM), Handlebars.js)
           </p>
+          <p>A social media application.</p>
           <div className="portfolio__item-cta">
             <a
               href="https://github.com/kazichaska/saysomething"
@@ -97,7 +153,8 @@ const Portfolio = () => {
           <h3>README.md Generator</h3>
           <p className="tech-used">(Node, Inquirer.js)</p>
           <p>
-            An application that generates professional quality README.md files for your GitHub projects using user inputs in the command line
+            An application that generates professional quality README.md files
+            for your GitHub projects using user inputs in the command line
           </p>
           <div className="portfolio__item-cta">
             <a
